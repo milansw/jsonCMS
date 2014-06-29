38cc1bf5ad90677726eaa0b7cdfbd60a441b6c09
Chris Ingram <chrisjamesingram@gmail.com>
2014-06-29 10:54:51 +0100

    added content json file and displayed on dashboard

--- dashboard.php
+++ dashboard.php
@@ -7,6 +7,8 @@
 
 	$f = file_get_contents('json/portfolio.json');
 	$json = json_decode($f);
+	$g = file_get_contents('json/content.json');
+	$cjson = json_decode($g);
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,54 +31,73 @@
 </head>
 
 <body>
-	<div class="container">
+	<header>
+		<div class="container">
 
-		<h1>jsonCMS</h1>
-		<div class="logout">Logged in as <strong><?php echo $_SESSION['user'] ?></strong>. <a href="logout.php">Logout</a></div>
+			<h1>jsonCMS</h1>
+			<div class="logout">Logged in as <strong><?php echo $_SESSION['user'] ?></strong>. <a href="logout.php">Logout</a></div>
 
-		<?php
-			if(isset($_SESSION['flash'])){
-				echo sprintf("<div class='alert alert-%s'>%s</div>",$_SESSION['flash']['type'],$_SESSION['flash']['message']);
-				unset($_SESSION['flash']);
-			}
-		?>
-
-		<div class="panel panel-default">
-			<div class="panel-body">
-				<form id="upload" class="form-horizontal" action="upload.php" method="post" enctype="multipart/form-data">
-					<label for="file" class="control-label">Filename:</label>
-					<input type="hidden" name="MAX_FILE_SIZE" value="10000000" /> 
-					<input type="file" name="upfile" id="upfile"><br>
-
-					<label for="caption" class="control-label">Caption: </label>
-					<input type="text" name="caption" class="form-control">
-					<input type="submit" name="submit" value="Upload" class="btn btn-primary">
-				</form>
-			</div>
-		</div>
-
-		<div class="row js-masonry" id="rowcontainer">
 			<?php
-				$count = 0;
-				foreach($json as $p){
-					echo "<div class='photo col-xs-6 col-sm-3'>"; //
-						echo "<div class='thumbnail'>";
-							echo "<a href='#'><img src='photos/thumb-" . $p->filename . "'></a>";
-							echo "<div class='caption'>";
-								echo "<p>" . $p->caption . "<br>";
-								echo "<small>" . date("F j Y, g:i a", $p->date_uploaded) . "</small></p>";
-								echo "<button class='btn btn-primary' data-toggle='modal' data-target='#modal".$count."'>Edit caption</button>";
-								echo "<a href='remove.php?fn=".$p->filename."' class='btn btn-danger'>Remove photo</a>";
-							echo "</div>";
-						echo "</div>";
-
-						include 'includes/editcaption_modal.php';
-
-					echo "</div>";
-					$count++;
+				if(isset($_SESSION['flash'])){
+					echo sprintf("<div class='alert alert-%s'>%s</div>",$_SESSION['flash']['type'],$_SESSION['flash']['message']);
+					unset($_SESSION['flash']);
 				}
 			?>
 		</div>
-	</div>
+	</header>
+
+	<section id="portfolio">
+		<div class="container">
+
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<form id="upload" class="form-horizontal" action="upload.php" method="post" enctype="multipart/form-data">
+						<label for="file" class="control-label">Filename:</label>
+						<input type="hidden" name="MAX_FILE_SIZE" value="10000000" /> 
+						<input type="file" name="upfile" id="upfile"><br>
+
+						<label for="caption" class="control-label">Caption: </label>
+						<input type="text" name="caption" class="form-control">
+						<input type="submit" name="submit" value="Upload" class="btn btn-primary">
+					</form>
+				</div>
+			</div>
+
+			<div class="row js-masonry" id="rowcontainer">
+				<?php
+					$count = 0;
+					foreach($json as $p){
+						echo "<div class='photo col-xs-6 col-sm-3'>"; //
+							echo "<div class='thumbnail'>";
+								echo "<a href='#'><img src='photos/thumb-" . $p->filename . "'></a>";
+								echo "<div class='caption'>";
+									echo "<p>" . $p->caption . "<br>";
+									echo "<small>" . date("F j Y, g:i a", $p->date_uploaded) . "</small></p>";
+									echo "<button class='btn btn-primary' data-toggle='modal' data-target='#modal".$count."'>Edit caption</button>";
+									echo "<a href='remove.php?fn=".$p->filename."' class='btn btn-danger'>Remove photo</a>";
+								echo "</div>";
+							echo "</div>";
+
+							include 'includes/editcaption_modal.php';
+
+						echo "</div>";
+						$count++;
+					}
+				?>
+			</div>
+		</div>
+	</section>
+
+	<section id="content">
+
+		<div class="container">
+
+			<div class="contentjson">
+				<?php print_r($cjson); ?>
+			</div>
+
+		</div>
+
+	</section>
 </body>
 </html>
